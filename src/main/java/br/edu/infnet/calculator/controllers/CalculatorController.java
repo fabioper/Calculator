136b5e8caf02ebc1c9b098d6014ece62412762b5
Fabio Pereira <fabiopereira.fps@gmail.com>
2020-03-12 02:59:49 -0300

    Add action listeners

--- src/main/java/br/edu/infnet/calculator/controllers/CalculatorController.java
+++ src/main/java/br/edu/infnet/calculator/controllers/CalculatorController.java
@@ -1,11 +1,59 @@
 package br.edu.infnet.calculator.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CalculatorController implements Initializable {
+    @FXML
+    void addValue(ActionEvent event) {
+
+    }
+
+    @FXML
+    void applyDivision(ActionEvent event) {
+
+    }
+
+    @FXML
+    void applyMultiplication(ActionEvent event) {
+
+    }
+
+    @FXML
+    void applyParentheses(ActionEvent event) {
+
+    }
+
+    @FXML
+    void applySubtraction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void applySum(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clearDisplay(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteInput(ActionEvent event) {
+
+    }
+
+    @FXML
+    void displayResult(ActionEvent event) {
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
