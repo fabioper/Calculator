f1ab90cccb0697bea35fa1c86c0850ec48ee1854
Fabio Pereira <fabiopereira.fps@gmail.com>
2020-03-12 03:03:08 -0300

    Optimize imports

--- src/main/java/br/edu/infnet/calculator/controllers/CalculatorController.java
+++ src/main/java/br/edu/infnet/calculator/controllers/CalculatorController.java
@@ -3,7 +3,6 @@ package br.edu.infnet.calculator.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
